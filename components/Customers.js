5dd2bdd88fb49af073f6656e7536217dde9bfc70
kahasan <kahraman.h@outlook.com>
2020-04-12 19:26:02 +0300

    Edit Customers section design for mobile

--- components/Customers.js
+++ components/Customers.js
@@ -10,19 +10,21 @@ function Customers() {
           subject="OUR CUSTOMERS"
           title="Illum qui dolorem eum quo voluptas ut distinctio"
         />
-        <Customercard
-          text="“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        <div className="Customers-container">
+          <Customercard
+            text="“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore ex magna aliqua. Ut enim
           illum ad minim veniam, quis ea exercitation ullamco cillum ut enim
           dolore.”"
-          img="/customer-1.png"
-          name="Cullen Bohannon"
-        />
-        <Customercard
-          text="“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi ipsam voluptatem aspernatur.”"
-          img="/customer-2.png"
-          name="Jennifer Thomas"
-        />
+            img="/customer-1.png"
+            name="Cullen Bohannon"
+          />
+          <Customercard
+            text="“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi ipsam voluptatem aspernatur.”"
+            img="/customer-2.png"
+            name="Jennifer Thomas"
+          />
+        </div>
       </div>
     </section>
   );
